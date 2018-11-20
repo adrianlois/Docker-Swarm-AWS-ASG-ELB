c283077aa672e84fb30fea6bf5d6ca805c0177b5
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2018-11-20 20:49:32 +0100

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,23 @@
+...
+/** El nombre de tu base de datos de WordPress */
+define('DB_NAME', 'wordpress');
+
+/** Tu nombre de usuario de MySQL */
+define('DB_USER', 'wordpress');
+
+/** Tu contraseña de MySQL */
+define('DB_PASSWORD', 'abc123.');
+
+/** Host de MySQL (es muy probable que no necesites cambiarlo) */
+define('DB_HOST', 'localhost');
+
+/** Codificación de caracteres para la base de datos. */
+define('DB_CHARSET', 'utf8');
+
+/** Cotejamiento de la base de datos. No lo modifiques si tienes dudas. */
+define('DB_COLLATE', '');
+
+/** Definir direcciones URLs **/
+define('WP_HOME','https://itgal.es');
+define('WP_SITEURL','https://itgal.es');
+...
